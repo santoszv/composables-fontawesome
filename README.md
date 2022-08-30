31f429d4c16e166e2b3ae3ffda22a3f2f7494eb9
Santos Zatarain Vera <coder.santoszv@gmail.com>
2022-08-29 20:49:20 -0500

    Version 6.1.2

---
+++ README.md
@@ -0,0 +1,46 @@
+# FontAwesome Composables
+
+FontAwesome SVG Icons as composables for Compose for Web.
+
+More info:
+
+- https://fontawesome.com/
+- https://www.jetbrains.com/lp/compose-mpp/
+
+## Usage
+
+Add to `build.gradle.kts`:
+
+```
+repositories {
+    ...
+    mavenCentral()
+    ...
+}
+
+dependencies {
+    ...
+    implementation("mx.com.inftel.oss:fontawesome-composables:<version>")
+    ...
+}
+```
+
+### Version compatibility
+
+| FontAwesome Composables | Compose for Web | KotlinJS |
+|-------------------------|-----------------|----------|
+| 6.1.2                   | 1.1.1           | 1.6.10   |
+
+## License
+
+Font Awesome Free License
+
+Font Awesome Free is free, open source, and GPL friendly. You can use it for
+commercial projects, open source projects, or really almost whatever you want.
+Full Font Awesome Free license: https://fontawesome.com/license/free.
+
+Icons: CC BY 4.0 License (https://creativecommons.org/licenses/by/4.0/)
+
+The Font Awesome Free download is licensed under a Creative Commons
+Attribution 4.0 International License and applies to all icons packaged
+as SVG and JS file types.
---
+++ build.gradle.kts
@@ -0,0 +1,147 @@
+import org.jetbrains.kotlin.util.capitalizeDecapitalize.capitalizeAsciiOnly
+
+buildscript {
+    dependencies {
+        classpath("org.glassfish:jakarta.json:2.0.1")
+    }
+}
+
+plugins {
+    kotlin("js") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.1"
+    `maven-publish`
+    signing
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.web.svg)
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings.optIn("org.jetbrains.compose.web.ExperimentalComposeWebSvgApi")
+    }
+    js(IR) {
+        browser()
+    }
+}
+
+val generateComposables by tasks.registering(DefaultTask::class) {
+    doLast {
+        file("src/main/kotlin/fontawesome").mkdirs()
+        file("src/main/kotlin/fontawesome/fontawesome.kt").bufferedWriter().use { writer ->
+            writer.appendLine("/*")
+            file("LICENSE.txt").readLines().forEach { writer.appendLine(it) }
+            writer.appendLine("*/")
+            writer.appendLine()
+            writer.appendLine("package fontawesome")
+            writer.appendLine()
+            writer.appendLine("import androidx.compose.runtime.Composable")
+            writer.appendLine("import org.jetbrains.compose.web.dom.AttrBuilderContext")
+            writer.appendLine("import org.jetbrains.compose.web.svg.Path")
+            writer.appendLine("import org.jetbrains.compose.web.svg.Svg")
+            writer.appendLine("import org.w3c.dom.svg.SVGElement")
+            file("icons.json").inputStream().use { input ->
+                val iconsMetadata = jakarta.json.Json.createParser(input).let {
+                    it.next()
+                    it.`object`
+                }
+                for ((iconName, iconMetadata) in iconsMetadata) {
+                    iconMetadata as jakarta.json.JsonObject
+                    val svgMetadata = iconMetadata.getJsonObject("svg")
+                    for ((styleName, svgData) in svgMetadata) {
+                        svgData as jakarta.json.JsonObject
+                        val identifier = run {
+                            val t1 = styleName.replace(Regex("\\W"), "_").split('_').joinToString("") { it.capitalizeAsciiOnly() }
+                            val t2 = iconName.replace(Regex("\\W"), "_").split('_').joinToString("") { it.capitalizeAsciiOnly() }
+                            "fa$t1$t2"
+                        }
+                        val viewBox = svgData.getJsonArray("viewBox")
+                        val path = svgData.getString("path")
+                        writer.appendLine()
+                        writer.appendLine("@Composable")
+                        writer.appendLine("fun ${identifier}(attrs: AttrBuilderContext<SVGElement>? = null) {")
+                        writer.appendLine("    Svg(viewBox = \"${viewBox.getInt(0)} ${viewBox.getInt(1)} ${viewBox.getInt(2)} ${viewBox.getInt(3)}\", attrs = {")
+                        writer.appendLine("        attr(\"aria-hidden\", \"true\")")
+                        writer.appendLine("        attr(\"focusable\", \"false\")")
+                        writer.appendLine("        attrs?.invoke(this)")
+                        writer.appendLine("    }) {")
+                        writer.appendLine("        Path(d = \"$path\")")
+                        writer.appendLine("    }")
+                        writer.appendLine("}")
+                    }
+                }
+            }
+        }
+    }
+}
+
+val jsJar by tasks.getting(Jar::class)
+
+val jsSourcesJar by tasks.getting(org.gradle.jvm.tasks.Jar::class)
+
+val jsJavadoc by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(file("$projectDir/files/README"))
+}
+
+publishing {
+    publications.register<MavenPublication>("js") {
+        artifact(jsJar)
+        artifact(jsSourcesJar)
+        artifact(jsJavadoc)
+        groupId = "mx.com.inftel.oss"
+        artifactId = "fontawesome-composables"
+        version = "6.1.2"
+        pom {
+            name.set("Composables FontAwesome Icons")
+            description.set("Composables FontAwesome Icons")
+            url.set("https://github.com/santoszv/composables-fontawesome")
+            inceptionYear.set("2022")
+            licenses {
+                license {
+                    name.set("Font Awesome Free License")
+                    url.set("https://fontawesome.com/license/free")
+                }
+                license {
+                    name.set("CC BY 4.0 License")
+                    url.set("https://creativecommons.org/licenses/by/4.0/")
+                }
+            }
+            developers {
+                developer {
+                    id.set("santoszv")
+                    name.set("Santos Zatarain Vera")
+                    email.set("santoszv@inftel.com.mx")
+                    url.set("https://www.inftel.com.mx")
+                }
+            }
+            scm {
+                connection.set("scm:git:https://github.com/santoszv/composables-fontawesome")
+                developerConnection.set("scm:git:https://github.com/santoszv/composables-fontawesome")
+                url.set("https://github.com/santoszv/composables-fontawesome")
+            }
+        }
+        signing.sign(this)
+    }
+
+    repositories {
+        maven {
+            setUrl(file("$projectDir/build/repo"))
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+}
+
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
+}
